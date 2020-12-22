f1217c5016d66d0da8c494cacb63a00d8c61b2c1
jhonedin <jhonedin1888@hotmail.com>
2020-12-22 14:12:39 -0500

    Cambios en Materias

---
+++ Materias/Pages/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <title>SISTEMA DE NOTAS</title>
+</head>
+<body> 
+    <h1>Materias </h1>
+    <a href="add.php" target="_blank">Registrar Materia</a>
+    <br><br>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Acciones</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Ingles</td>
+            <td>
+                <a href="edit.php" target="_blank">Editar</a>
+                <a href="delete.php" target="_blank">Eliminar</a>
+            </td>            
+        </tr>
+    </table>
+
+</body>
+</html>
