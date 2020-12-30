795c07e224af69b6a1df26e09cc5a72a5fa101e5
jhonedin <jhonedin1888@hotmail.com>
2020-12-30 17:49:15 -0500

    Funcionalidades Controlador Materias

--- Materias/Modelo/Materias.php
+++ Materias/Modelo/Materias.php
@@ -8,23 +8,60 @@ class Materias extends conexion{
         $this->db = parent::__construct(); // de la clase padre heredamos el constructor. 
     }
 
-    public function add(){
+    public function add($Materia){
+        $statement = $this->db->prepare("INSERT INTO materias (MATERIA) VALUES (:Materia) ");
+        $statement->bindParam(':Materia',$Materia);
+        if($statement->execute()){
+            header('Location: ../Pages/index.php');
+        }else{
+            header('Location: ../Pages/add.php');
+        }
 
     }
 
     public function get(){
+        $rows = null;
+        $statement = $this->db->prepare("SELECT * FROM materias");
+        $statement->execute();
+        while($result = $statement->fetch()){
+            $rows[] = $result;
+        }
+        return $rows;
 
     }
 
-    public function getById(){
+    public function getById($Id){
+        $rows = null;
+        $statement = $this->db->prepare("SELECT * FROM materias WHERE ID_MATERIA = :Id");
+        $statement->bindParam(':Id',$Id);
+        $statement->execute();
+        while($result = $statement->fetch()){
+            $rows[] = $result;
+        }
+        return $rows;
 
     }
 
-    public function update(){
+    public function update($Id, $Materia){
+        $statement = $this->db->prepare("UPDATE materias SET MATERIA = :Materia WHERE ID_MATERIA = :Id");
+        $statement->bindParam(':Id',$Id);
+        $statement->bindParam(':Materia',$Materia);
+        if($statement->execute()){
+            header('Location: ../Pages/index.php');
+        }else{
+            header('Location: ../Pages/edit.php');
+        }
 
     }
 
-    public function delete(){
+    public function delete($Id){
+        $statement = $this->db->prepare("DELETE FROM materias WHERE ID_MATERIA = :Id");
+        $statement->bindParam(':Id',$Id);
+        if($statement->execute()){
+            header('Location: ../Pages/index.php');
+        }else{
+            header('Location: ../Pages/delete.php');
+        } 
         
     }
 
