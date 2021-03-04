f72c2ca2fa0e9491435666b85cb16373b72f71d7
jhonedin <jhonedin1888@hotmail.com>
2021-03-03 21:01:42 -0500

    Etiquetas header, main, y footer en el index de login

--- index.php
+++ index.php
@@ -3,15 +3,23 @@
     <title>SISTEMA DE NOTAS</title>
 </head>
 <body> 
-    <h1>Inicio de Sesión </h1>
-    <form method="POST" action="Usuarios/Controladores/Login.php">
-    Usuario: <br>
-    <input type="text" name="Usuario" required="" autocomplete="off" placeholder="Usuario"> 
-    <br><br>
-    Contrasena: <br>
-    <input type="password" name="Contrasena" required="" autocomplete="off" placeholder="Contraseña">
-    <br><br>
-    <input type="submit" value="Inicia Sesion">
-    </form>
+    <header>
+    <h1>SISTEMAS DE NOTAS</h1>
+    </header>
+    <main>
+        <h2>Inicio de Sesion</h2>
+        <form method="POST" action="Usuarios/Controladores/Login.php">
+        Usuario: <br>
+        <input type="text" name="Usuario" required="" autocomplete="off" placeholder="Usuario"> 
+        <br><br>
+        Contrasena: <br>
+        <input type="password" name="Contrasena" required="" autocomplete="off" placeholder="Contraseña">
+        <br><br>
+        <input type="submit" value="Inicia Sesion">
+        </form>
+    </main>
+    <footer>
+        <h4>Espacio para el pie de pagina</h4>
+    </footer>
 </body>
 </html>
