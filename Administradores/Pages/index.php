551c22f2a30d2f39ec3423610b7ab7d3a6dcbb13
jhonedin <jhonedin1888@hotmail.com>
2021-03-16 21:19:25 -0500

    Ajustados los estilos del index del modulo de administradores

--- Administradores/Pages/index.php
+++ Administradores/Pages/index.php
@@ -9,7 +9,15 @@
 
 <!DOCTYPE html>
 <html>
-<head> 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">     
     <title>SISTEMA DE NOTAS</title>
     <link rel="stylesheet" href="../../styles.css">
 </head>
@@ -18,56 +26,68 @@
         <h1>SISTEMAS DE NOTAS </h1>
     </header>
     <nav>
-        <ul>
-            <li><a href="#">Administradores</a></li>
-            <li><a href="../../Docentes/Pages/index.php">Docentes</a></li>
-            <li><a href="../../Materias/Pages/index.php">Materias</a></li>
-            <li><a href="../../Estudiantes/Pages/index.php">Estudiantes</a></li>
-            <li><a href="../../Usuarios/Controladores/salir.php">Salir</a></li>
+        <ul class="main-nav">
+            <li class="main-nav__item"><a href="#">Administradores</a></li>
+            <li class="main-nav__item"><a href="../../Docentes/Pages/index.php">Docentes</a></li>
+            <li class="main-nav__item"><a href="../../Materias/Pages/index.php">Materias</a></li>
+            <li class="main-nav__item"><a href="../../Estudiantes/Pages/index.php">Estudiantes</a></li>
+            <li class="main-nav__item"><a href="../../Usuarios/Controladores/salir.php">Salir</a></li>
         </ul>
     </nav>
-    <h3>Usted ha iniciado sesión como: <?php echo $ModeloUsuarios->getNombre()?> - <?php echo $ModeloUsuarios->getPerfil()?> </h3>
+    <h3 class="info-sesion">Usted ha iniciado sesión como: <?php echo $ModeloUsuarios->getNombre()?> - <?php echo $ModeloUsuarios->getPerfil()?> </h3>
     <main>
-        <h2>Administradores </h2>
-        <a href="add.php" target="_blank">Registrar Administrador</a>
-        <br><br>
-        <table border="1">
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Usuario</th>
-                <th>Perfil</th>
-                <th>Estado</th>
-                <th>Acciones</th>
-            </tr>
-            <?php 
-                $Administradores = $ModeloAdministradores->get();
-                // Validamos que existan administradores
-                if($Administradores != null){
-                    foreach($Administradores as $Administrador){
+        <div class="modulo-presente">
+            <h2>Modulo Administradores </h2>
+        </div>
+        <div class="box-add">
+            <a href="add.php" target="_blank">Registrar Administrador</a>
+        </div>
+        <br>
+        <div class="table-box">
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Usuario</th>
+                    <th>Perfil</th>
+                    <th>Estado</th>
+                    <th>Acciones</th>
+                </tr>
+                <?php 
+                    $Administradores = $ModeloAdministradores->get();
+                    // Validamos que existan administradores
+                    if($Administradores != null){
+                        foreach($Administradores as $Administrador){
 
-            ?>
-            <tr>
-                <td><?php echo $Administrador['ID_USUARIO'] ?></td>
-                <td><?php echo $Administrador['NOMBRE'] ?></td>
-                <td><?php echo $Administrador['APELLIDO'] ?></td>
-                <td><?php echo $Administrador['USUARIO'] ?></td>
-                <td><?php echo $Administrador['PERFIL'] ?></td>
-                <td><?php echo $Administrador['ESTADO'] ?></td>
-                <td>
-                    <a href="edit.php?Id=<?php echo $Administrador['ID_USUARIO'] ?>" target="_blank">Editar</a>
-                    <a href="delete.php?Id=<?php echo $Administrador['ID_USUARIO'] ?>" target="_blank">Eliminar</a>
-                </td>   
-            </tr>
-            <?php 
+                ?>
+                <tr>
+                    <td><?php echo $Administrador['ID_USUARIO'] ?></td>
+                    <td><?php echo $Administrador['NOMBRE'] ?></td>
+                    <td><?php echo $Administrador['APELLIDO'] ?></td>
+                    <td><?php echo $Administrador['USUARIO'] ?></td>
+                    <td><?php echo $Administrador['PERFIL'] ?></td>
+                    <td><?php echo $Administrador['ESTADO'] ?></td>
+                    <td>
+                        <div>
+                            <ul class="nav-edit-delete">
+                                <li class="nav-edit-delete_item-edit"><a href="edit.php?Id=<?php echo $Administrador['ID_USUARIO'] ?>" target="_blank">Editar</a>
+                                <li class="nav-edit-delete_item-delete"><a href="delete.php?Id=<?php echo $Administrador['ID_USUARIO'] ?>" target="_blank">Eliminar</a>
+                            </ul>
+                        </div>
+                    </td>   
+                </tr>
+                <?php 
+                        }
                     }
-                }
-            ?>
-        </table>
+                ?>
+            </table>
+        <div>
     </main>
     <footer>
-        <h4>Espacio para el pie de pagina</h4>
+        <h4>Powered by</h4>
+        <h5>JHON EDINSON BLANDON</h5>
+        <h6>2021</h6>
     </footer>
 
 </body>
