d8b7006e3f2ed7c9f9e8c1a8b5c09a7a58f8cbf3
jhonedin <jhonedin1888@hotmail.com>
2021-03-16 21:54:58 -0500

    Ajustados los estilos del index del modulo docentes

--- Docentes/Pages/index.php
+++ Docentes/Pages/index.php
@@ -8,65 +8,85 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head> 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">    
     <title>SISTEMA DE NOTAS</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../../styles.css">
 </head>
 <body>
     <header> 
         <h1>SISTEMAS DE NOTAS </h1>
     </header>
     <nav>
-        <ul>
-            <li><a href="../../Administradores/Pages/index.php">Administradores</a></li>
-            <li><a href="#">Docentes</a></li>
-            <li><a href="../../Materias/Pages/index.php">Materias</a></li>
-            <li><a href="../../Estudiantes/Pages/index.php">Estudiantes</a></li>
-            <li><a href="../../Usuarios/Controladores/salir.php">Salir</a></li>
+        <ul class="main-nav">
+            <li class="main-nav__item"><a href="../../Administradores/Pages/index.php">Administradores</a></li>
+            <li class="main-nav__item"><a href="#">Docentes</a></li>
+            <li class="main-nav__item"><a href="../../Materias/Pages/index.php">Materias</a></li>
+            <li class="main-nav__item"><a href="../../Estudiantes/Pages/index.php">Estudiantes</a></li>
+            <li class="main-nav__item"><a href="../../Usuarios/Controladores/salir.php">Salir</a></li>
         </ul>
     </nav>
-    <h3>Usted ha iniciado sesión como: <?php echo $ModeloUsuarios->getNombre()?> - <?php echo $ModeloUsuarios->getPerfil()?> </h3>
+    <h3 class="info-sesion">Usted ha iniciado sesión como: <?php echo $ModeloUsuarios->getNombre()?> - <?php echo $ModeloUsuarios->getPerfil()?> </h3>
     <main>
-        <h2>Docentes </h2>
+        <div class="modulo-presente">
+            <h2>Modulo Docentes </h2>
+        </div>
+        <div class="box-add">
         <a href="add.php" target="_blank">Registrar Docente</a>
-        <br><br>
-        <table border="1">
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Usuario</th>
-                <th>Perfil</th>
-                <th>Estado</th>
-                <th>Acciones</th>
-            </tr>
-            <?php 
-                $Docentes = $ModeloDocentes->get();
-                // Validamos que existan Docentes
-                if($Docentes != null){
-                    foreach($Docentes as $Docente){
+        </div>
+        <br>
+        <div class="table-box">
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Usuario</th>
+                    <th>Perfil</th>
+                    <th>Estado</th>
+                    <th>Acciones</th>
+                </tr>
+                <?php 
+                    $Docentes = $ModeloDocentes->get();
+                    // Validamos que existan Docentes
+                    if($Docentes != null){
+                        foreach($Docentes as $Docente){
 
-            ?>
-            <tr>
-                <td><?php echo $Docente['ID_USUARIO'] ?></td>
-                <td><?php echo $Docente['NOMBRE'] ?></td>
-                <td><?php echo $Docente['APELLIDO'] ?></td>
-                <td><?php echo $Docente['USUARIO'] ?></td>
-                <td><?php echo $Docente['PERFIL'] ?></td>
-                <td><?php echo $Docente['ESTADO'] ?></td>
-                <td>
-                    <a href="edit.php?Id=<?php echo $Docente['ID_USUARIO'] ?>" target="_blank">Editar</a>
-                    <a href="delete.php?Id=<?php echo $Docente['ID_USUARIO'] ?>" target="_blank">Eliminar</a>
-                </td>   
-            </tr>
-            <?php 
+                ?>
+                <tr>
+                    <td><?php echo $Docente['ID_USUARIO'] ?></td>
+                    <td><?php echo $Docente['NOMBRE'] ?></td>
+                    <td><?php echo $Docente['APELLIDO'] ?></td>
+                    <td><?php echo $Docente['USUARIO'] ?></td>
+                    <td><?php echo $Docente['PERFIL'] ?></td>
+                    <td><?php echo $Docente['ESTADO'] ?></td>
+                    <td>
+                        <div>
+                            <ul class="nav-edit-delete">
+                                <li class="nav-edit-delete_item-edit"><a href="edit.php?Id=<?php echo $Docente['ID_USUARIO'] ?>" target="_blank">Editar</a></li>
+                                <li class="nav-edit-delete_item-delete"><a href="delete.php?Id=<?php echo $Docente['ID_USUARIO'] ?>" target="_blank">Eliminar</a></li>
+                            </ul>
+                        </div>
+                    </td>   
+                </tr>
+                <?php 
+                        }
                     }
-                }
-            ?>
-        </table>
+                ?>
+            </table>
+        <div>
     <main>
     <footer>
-        <h4>Espacio para el pie de pagina</h4>
+        <h4>Powered by</h4>
+        <h5>JHON EDINSON BLANDON</h5>
+        <h6>2021</h6>
     </footer>
    
 
