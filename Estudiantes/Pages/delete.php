193824923fb83091d2edd666d11d355aab4ea4d3
jhonedin <jhonedin1888@hotmail.com>
2021-03-25 23:28:48 -0500

    Añadidos los estilos de eliminar estudiante

--- Estudiantes/Pages/delete.php
+++ Estudiantes/Pages/delete.php
@@ -9,13 +9,25 @@
 <!DOCTYPE html>
 <html>
 <head> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">  
     <title>SISTEMA DE NOTAS</title>
+    <link type="text/css" rel="stylesheet" href="../../styles.css">
 </head>
 <body> 
-    <h1>Eliminar Estudiante </h1>
-    <form method="POST" action="../Controladores/delete.php">
+    <header> 
+        <h1>SISTEMAS DE NOTAS </h1>
+    </header> 
+    <h2 class="title-createnew">Eliminar Estudiantes</h2>
+    <form class="formularioEstudiante" method="POST" action="../Controladores/delete.php">
         <input type="hidden" name="Id" value="<?php echo $Id ?>">
-        <p>¿Estas seguro que deseas eliminar este estudiante?</p>
+        <p id="textEliminarEstudiante">¿Estas seguro que deseas eliminar este estudiante?</p>
         <input type="submit" value="Eliminar Estudiante">
     </form>
 </body>
